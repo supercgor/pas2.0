4a658b81f226f894b03124e4a5dbe5413d6bb22b
See.Looooo <siumabon123@gmail.com>
2022-03-07 10:39:29 +0800

    0.2

--- func.py
+++ func.py
@@ -12,6 +12,16 @@ import warnings
 import json
 warnings.filterwarnings('ignore')
 
+def dropdown_handler(driver, xpath: str):
+    """
+    点击带有滚动条的菜单
+    ref: https://stackoverflow.com/questions/57303355
+    """
+    wait = WebDriverWait(driver, 10)
+    ele = wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+    ele.location_once_scrolled_into_view
+    ele.click()
+    time.sleep(0.1)
 
 def login(driver, userName, password, retry=0):
     if retry == 3:
@@ -92,34 +102,25 @@ def select_in_out(driver, way):
 
 def select_campus(driver, campus):
     driver.find_elements_by_class_name('el-select')[1].click()
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(
-            (By.XPATH, f'//li/span[text()="{campus}"]')))
-    driver.find_element_by_xpath(f'//li/span[text()="{campus}"]').click()
+    dropdown_handler(driver, f'//li/span[text()="{campus}"]')
 
-    
+
 def write_reason(driver, reason):
     driver.find_elements_by_class_name('el-select')[2].click()
     WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located(
             (By.XPATH, f'//li/span[text()="{reason}"]')))
     driver.find_element_by_xpath(f'//li/span[text()="{reason}"]').click()
-    
-    
+
+
 def select_destination(driver, destination):
     driver.find_elements_by_class_name('el-select')[3].click()
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(
-            (By.XPATH, f'//li/span[text()="{destination}"]')))
-    driver.find_element_by_xpath(f'//li/span[text()="{destination}"]').click()
+    dropdown_handler(driver, f'//li/span[text()="{destination}"]')
 
 
 def select_district(driver, district):
     driver.find_elements_by_class_name('el-select')[4].click()
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(
-            (By.XPATH, f'//li/span[text()="{district}"]')))
-    driver.find_element_by_xpath(f'//li/span[text()="{district}"]').click()
+    dropdown_handler(driver, xpath=f'//li/span[text()="{district}"]')
 
 
 def write_mail_address(driver, mail_address):
@@ -134,8 +135,8 @@ def write_phone_number(driver, phone_number):
     driver.find_elements_by_class_name('el-input__inner')[3].send_keys(
         f'{phone_number}')
     time.sleep(0.1)
-    
-    
+
+
 def write_reason_detail(driver, detail):
     driver.find_element_by_class_name('el-textarea__inner').send_keys(
         f'{detail}')
@@ -177,116 +178,76 @@ def submit(driver):
 
 def fill_out(driver, campus, mail_address, phone_number, reason, detail, destination, track):
     print('开始填报出校备案')
-
     print('选择出校/入校    ', end='')
     select_in_out(driver, '出校')
-    print('Done')
-
     print('选择校区    ', end='')
     select_campus(driver, campus)
-    print('Done')
-
     print('填写邮箱    ', end='')
     write_mail_address(driver, mail_address)
-    print('Done')
-
     print('填写手机号    ', end='')
     write_phone_number(driver, phone_number)
-    print('Done')
-
     print('填写出入校事由    ', end='')
     write_reason(driver, reason)
-    print('Done')
-    
     print('填写出入校事由详细描述    ', end='')
     write_reason_detail(driver, detail)
-    print('Done')
-
     print('选择出校目的地    ', end='')
     select_destination(driver, destination)
-    print('Done')
-
     print('填写出校行动轨迹    ', end='')
     write_track(driver, track)
-    print('Done')
-
     click_check(driver)
     submit(driver)
-
     print('出校备案填报完毕！')
 
 
 def fill_in(driver, campus, mail_address, phone_number, reason, detail, habitation, district, street):
     print('开始填报入校备案')
-
     print('选择出校/入校    ', end='')
     select_in_out(driver, '入校')
-    print('Done')
-
     print('选择校区    ', end='')
     select_campus(driver, campus)
-    print('Done')
-
     print('填写邮箱    ', end='')
     write_mail_address(driver, mail_address)
-    print('Done')
-
     print('填写手机号    ', end='')
     write_phone_number(driver, phone_number)
-    print('Done')
-
     print('填写出入校事由    ', end='')
     write_reason(driver, reason)
-    print('Done')
-    
     print('填写出入校事由详细描述    ', end='')
     write_reason_detail(driver, detail)
-    print('Done')
-
     if habitation != '北京':
         raise Exception('暂不支持京外入校备案，请手动填写')
-
     print('选择居住地所在区    ', end='')
     select_district(driver, district)
-    print('Done')
-
     print('填写居住地所在街道    ', end='')
     write_street(driver, street)
-    print('Done')
-
     click_inPeking(driver)
     click_check(driver)
     submit(driver)
-
     print('入校备案填报完毕！')
 
 
-def screen_capture(driver, path):
-    driver.back()
-    time.sleep(0.5)
-    driver.back()
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, 'el-card__body')))
-    driver.find_elements_by_class_name('el-card__body')[1].click()
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(
-            (By.XPATH, '//div[contains(text(),"已提交")]')))
-    driver.maximize_window()
-    time.sleep(0.1)
-    driver.save_screenshot(os.path.join(path, 'result.png'))
-    print('备案历史截图已保存')
-
-
-def wechat_notification(userName, sckey):
+def wechat_notification_success(userName, sckey):
     with request.urlopen(
             quote('https://sctapi.ftqq.com/' + sckey + '.send?title=成功报备&desp=学号' +
                   str(userName) + '成功报备',
                   safe='/:?=&')) as response:
         response = json.loads(response.read().decode('utf-8'))
-    # if response['error'] == 'SUCCESS':
-    #     print('微信通知成功！')
-    # else:
-    #     print(str(response['errno']) + ' error: ' + response['errmsg'])
+
+
+def wechat_notification_failed(userName, sckey):
+    with request.urlopen(
+            quote('https://sctapi.ftqq.com/' + sckey + '.send?title=[请注意]报备失败&desp=学号' +
+                  str(userName) + '报备失败，需要您手动报备或在github页面手动重新运行，您可以在主仓库issue下报告错误日志。',
+                  safe='/:?=&')) as response:
+        response = json.loads(response.read().decode('utf-8'))
+
+
+def exception_printer(e: Exception or None):
+    print_bold = lambda x: print('\033[1;31m' + x + '\033[0m', file=sys.stderr)
+    print_bold("多次重试失败，报备失败")
+    print_bold('请检查您的配置是否正确，或者稍后重试')
+    print_bold('如果错误依然存在，请在这里汇报Bug：https://github.com/xiazhongyv/PKUAutoSubmit_online/issues')
+    print_bold('错误详细信息：')
+    raise e
 
 
 def run(driver, userName, password, campus, mail_address, phone_number, reason, detail, destination, track,
@@ -302,12 +263,8 @@ def run(driver, userName, password, campus, mail_address, phone_number, reason, 
     fill_in(driver, campus, mail_address, phone_number, reason, detail, habitation, district, street)
     print('=================================')
 
-    if capture:
-        screen_capture(driver, path)
-        print('=================================')
-
     if wechat:
-        wechat_notification(userName, sckey)
+        wechat_notification_success(userName, sckey)
         print('=================================')
 
     print('报备成功！\n')
